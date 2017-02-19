d4fd3563ec5b7fb8eb767ad96b5d892e728811d1
mrkumar0 <maniranjan0@gmail.com>
2017-02-19 14:41:28 +0530

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -2,7 +2,7 @@
 	date_default_timezone_set('America/New_York');
 	$dbhost = 'localhost';
 	$dbuser = 'root';
-	$dbpass = '29119work';
+	$dbpass = 'password';
 	$dbname = 'search';
 
 	$connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die(mysqli_error($connect));
